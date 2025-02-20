5c3ae5db9ac39eb6c436628889cf540ce6851a4f
Srinivas Jadcharla <jdj2ee@gmail.com>
2025-02-19 22:09:11 -0700

    Trees-4

---
+++ LowestCommonAncestorSolution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+class LowestCommonAncestorSolution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+
+        while (true) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+
+    }
+}
